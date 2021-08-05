abeedd3e154b89d59c854ad9ce73ea92bb58a029
Iago Freitas <iagofreitas05@gmail.com>
2021-08-04 23:32:53 -0300

    1 - init of the project

---
+++ src/components/header/Navigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="nav" class="p-8 mt-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex justify-between space-x-4">
+    <div ><router-link class="text-center" to="/">Home</router-link></div>
+    <div ><router-link class="text-center" to="/about">Sobre</router-link></div>
+    <div ><router-link class="text-center" to="/register">Registrar</router-link></div>
+    <div ><router-link class="text-center" to="/login">login</router-link></div>
+  </div>
+
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  color: #2c3e50;
+}
+
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+
---
+++ src/views/Register.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="mt-8">
+    <h1 class="text-xl font-medium text-black" >página de registro</h1>
+  </div>
+</template>
