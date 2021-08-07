73619cf65d7473b46926cf4c2a1bdadfbba9e060
Iago Freitas <iagofreitas05@gmail.com>
2021-08-06 21:14:16 -0300

    3 - register page made

--- src/components/header/Navigation.vue
+++ src/components/header/Navigation.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="nav" class="p-8 mt-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex justify-between space-x-4">
-    <div ><router-link class="text-center" to="/">Home</router-link></div>
-    <div ><router-link class="text-center" to="/about">Sobre</router-link></div>
-    <div ><router-link class="text-center" to="/register">Registrar</router-link></div>
+    <div ><router-link class="text-center" to="/store">loja</router-link></div>
+    <div ><router-link class="text-center" to="/about">sobre</router-link></div>
+    <div ><router-link class="text-center" to="/register">registrar</router-link></div>
     <div ><router-link class="text-center" to="/login">login</router-link></div>
+
   </div>
 
   <router-view/>
@@ -25,7 +26,7 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #7C3AED;
 }
 </style>
 
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,139 @@
 <template>
-  <div class="mt-8">
-    <h1 class="text-xl font-medium text-black" >página de registro</h1>
+  <div class="mt-8  ">
+    <div class=" w-screen max-w-lg   mx-auto">
+      <form class="bg-white shadow-md rounded  px-8 pt-6 pb-8 mb-4">
+        <h4 class="text-xl font-bold text-purple-600 my-4">dados pessoais</h4>
+        <div class="mb-4">
+          <div class="flex">
+            <div class="w-1/2 p-1">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                nome
+              </label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="nome">
+            </div>
+            <div class="w-1/2 p-1">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                sobrenome
+              </label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="last_name" type="text" placeholder="sobrenome">
+            </div>
+          </div>
+        </div>
+        <div class="mb-4">
+          <div class="flex">
+            <div class="w-1/2 p-1">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                data de nascimento
+              </label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="dt_nascimento" type="date" placeholder="nome">
+            </div>
+            <div class="w-1/2 p-1">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                telefone
+              </label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="cellphone" type="tel" pattern="[0-9]{2}-[0-9]{5}-[0-9]{4}" placeholder="(dd) xxxxx-xxxx">
+            </div>
+          </div>
+        </div>
+        <div class="mb-4">
+          <div class="flex">
+            <div class="w-1/2 p-1">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                gênero
+              </label>
+              <select class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="gender"  >
+                <option value="">gênero</option>
+                <option value="masculino">masculino</option>
+                <option value="feminino">feminino</option>
+                <option value="na">não binário</option>
+              </select>
+            </div>
+            <div class="w-1/2 p-1">
+              <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                cpf
+              </label>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="cpf" type="text" placeholder="cpf">
+            </div>
+          </div>
+        </div>
+
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            email
+          </label>
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" placeholder="email">
+        </div>
+        <div class="mb-6">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+            senha
+          </label>
+          <input class="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+        </div>
+        <h4 class="text-xl font-bold text-purple-600 my-4">endereço</h4>
+        <div class="mb-4 ">
+          <label class="block text-gray-700 text-sm font-bold mb-2 " for="username">
+            logradouro
+          </label>
+          <div class="flex">
+            <div class="w-5/6 p-1">
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="rua, avenida, etc">
+            </div>
+            <div class="w-1/6 p-1">
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="numero" type="number" placeholder="nº">
+            </div>
+          </div>
+        </div>
+        <div class="mb-6">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+            localização
+          </label>
+          <div class="flex">
+            <div class="w-full p-1">
+              <input class="shadow appearance-none border  rounded w-full   py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="city" type="text" placeholder="cidade">
+            </div>
+            <div class="p-1">
+              <select class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="state" type="text" placeholder="">
+                <option value="">Selecione</option>
+                <option value="AC">Acre</option>
+                <option value="AL">Alagoas</option>
+                <option value="AP">Amapá</option>
+                <option value="AM">Amazonas</option>
+                <option value="BA">Bahia</option>
+                <option value="CE">Ceará</option>
+                <option value="DF">Distrito Federal</option>
+                <option value="ES">Espirito Santo</option>
+                <option value="GO">Goiás</option>
+                <option value="MA">Maranhão</option>
+                <option value="MS">Mato Grosso do Sul</option>
+                <option value="MT">Mato Grosso</option>
+                <option value="MG">Minas Gerais</option>
+                <option value="PA">Pará</option>
+                <option value="PB">Paraíba</option>
+                <option value="PR">Paraná</option>
+                <option value="PE">Pernambuco</option>
+                <option value="PI">Piauí</option>
+                <option value="RJ">Rio de Janeiro</option>
+                <option value="RN">Rio Grande do Norte</option>
+                <option value="RS">Rio Grande do Sul</option>
+                <option value="RO">Rondônia</option>
+                <option value="RR">Roraima</option>
+                <option value="SC">Santa Catarina</option>
+                <option value="SP">São Paulo</option>
+                <option value="SE">Sergipe</option>
+                <option value="TO">Tocantins</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="flex items-center justify-between">
+          <button class="bg-blue-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            cadastrar
+          </button>
+        </div>
+      </form>
+      <p class="text-center text-gray-500 text-xs">
+        &copy;2021 Epona Sports. Todos os direitos reservados.
+      </p>
+  </div>
   </div>
 </template>
