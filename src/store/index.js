4961617b377334322a805b5f3c09091b0ccd3dd0
Iago Freitas <iagofreitas05@gmail.com>
2021-08-27 01:10:19 -0300

    1- registro feito com sucesso. 2 - Autenticação de usuário feita com sucesso.

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,19 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    auth:{
+      token:"false"
+    }
   },
   mutations: {
+    saveToken(state, payload){
+      state.auth.token = payload
+    }
   },
   actions: {
+    saveToken(context, payload){
+      context.commit('saveToken', payload)
+    }
   },
   modules: {
   }
--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,22 +1,45 @@
 <template>
   <div class="mt-8  ">
     <div class=" w-screen max-w-lg   mx-auto">
-      <form class="bg-white shadow-md rounded  px-8 pt-6 pb-8 mb-4">
+      <form @submit.prevent="storeLogin" class="bg-white shadow-md rounded  px-8 pt-6 pb-8 mb-4">
         <h4 class="text-xl font-bold text-purple-600 my-4">acesse sua conta</h4>
         <div class="mb-4">
           <label class="block text-gray-700 text-sm font-bold mb-2" >
             email
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline required" name="email" id="email" type="email" placeholder="email">
+          <input
+              class="shadow appearance-none
+                border rounded w-full py-2 px-3 text-gray-700
+                  leading-tight focus:outline-none
+                    focus:shadow-outline required"
+              name="email"
+              id="email"
+              type="email"
+              placeholder="email"
+              v-model="mail"
+              required
+          >
         </div>
         <div class="mb-6">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
             senha
           </label>
-          <input class="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline required" name="password" id="password" type="password" placeholder="******************">
+          <input
+              class="shadow appearance-none border
+              rounded w-full py-2 px-3 text-gray-700 mb-3
+              leading-tight focus:outline-none
+              focus:shadow-outline
+              required"
+              name="password"
+              id="password"
+              type="password"
+              placeholder="******************"
+              v-model="password"
+              required
+          >
         </div>
         <div class="flex items-center justify-between">
-          <button @click="loginNotification" id="enter" class="bg-blue-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" name="login" type="button">
+          <button @click="chargeOn" id="enter" class="bg-blue-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" name="login" type="submit">
             <span v-if="charge ===  true ">acessando...</span>
             <span v-else-if="charge === false">acessar</span>
           </button>
@@ -36,12 +59,32 @@ export default{
   name:"Login",
   data(){
     return{
+      mail:"",
+      password:"",
       charge:false,
     }
   },
   methods:{
     chargeOn(){
       this.charge = !this.charge
+    },
+    storeLogin(){
+      this.axios.post("http://localhost:8080/place/auth",{
+        mail:this.mail,
+        password: this.password,
+        token:""
+      })
+        .then((response)=>{
+          response.data
+          swal("Uhuu 0/","Login feito com sucesso","success")
+              .then(() => {
+                this.token = response.data.token
+                this.$store.dispatch("saveToken", this.token)
+                this.$router.push("Profile")
+              });
+        }).catch(() =>{
+          swal("Oops :(","parece que seus dados estão incorretos.","error")
+      })
     },
     loginNotification(){
       swal("tá dentro", "qual é a boa de hoje?", "success");
