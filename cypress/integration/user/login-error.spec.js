a841a7fa64b33e7bab9733b0e201154ce903d6b0
Iago Freitas <iagofreitas05@gmail.com>
2021-08-29 16:36:25 -0300

    1- log de erros

--- cypress/integration/user/login-error.spec.js
+++ cypress/integration/user/login-error.spec.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
 ///<reference types="cypress" />
 
-describe('Login',()=>{
-    
-    it('Realiza login',()=>{
+describe('LoginError',()=>{
+
+    it('Realiza login com erro',()=>{
         cy.visit('http://localhost:8081/#/login')
-        cy.get('input[name=email]').type('iagofreitas05@gmail.com');
+        cy.get('input[name=email]').type('testedeerro@erro.com');
         cy.get('input[name=password]').type('123');
         cy.contains('acessar').click();
     });
