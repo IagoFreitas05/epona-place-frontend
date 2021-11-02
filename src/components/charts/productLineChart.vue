b4449174d686e5038c58b63994cd63c0c4fdc75f
Iago Freitas <iagofreitas05@gmail.com>
2021-11-01 21:44:17 -0300

    1- mockup dos gráficos: categoria, produto, vendas por mes

---
+++ src/components/charts/productLineChart.vue
@@ -0,0 +1,69 @@
+<template>
+  <LineChart :chartData="testData" />
+</template>
+
+<script lang="ts">
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+
+import { defineComponent } from 'vue';
+import { LineChart } from 'vue-chart-3';
+export default defineComponent({
+  name: 'productLineChart',
+  components: { LineChart },
+  setup() {
+    const testData = {
+      labels: ['janeiro','fevereiro','marco','abril','maio','junho','julho'],
+      datasets: [{
+        label: 'carro',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(201, 203, 207, 0.2)'
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)'
+        ],
+        borderWidth: 3
+      },
+        {
+          label: 'moto',
+          data: [25, 49, 20, 61, 56, 25, 0],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+          ],
+          borderWidth: 3
+        }
+      ]
+    };
+
+    return { testData };
+  },
+});
+</script>
--- src/views/admin/ProfileAdmin.vue
+++ src/views/admin/ProfileAdmin.vue
@@ -5,45 +5,85 @@
         <div class="flex flex-wrap -m-4">
           <div class="xl:w-1/3 md:w-1/2 p-4">
             <div class="border bg-white border-gray-200 p-6 rounded-lg">
-              <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <div
+                  class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                     class="w-6 h-6" viewBox="0 0 24 24">
                   <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                 </svg>
               </div>
               <h2 class="text-lg text-gray-900 font-medium title-font mb-2">aguardando envio</h2>
-              <p class="leading-relaxed text-base">20</p>
+              <p class="leading-relaxed text-base">{{andamento}}</p>
             </div>
           </div>
           <div class="xl:w-1/3 md:w-1/2 p-4">
             <div class="border  bg-white border-gray-200 p-6 rounded-lg">
-              <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+              <div
+                  class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                 <svg class="svg-icon" viewBox="0 0 20 20">
-                  <path fill="none" d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
-                  <path fill="none" d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
-                  <path fill="none" d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
+                  <path fill="none"
+                        d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
+                  <path fill="none"
+                        d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
+                  <path fill="none"
+                        d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
                 </svg>
               </div>
               <h2 class="text-lg text-gray-900 font-medium title-font mb-2">enviados</h2>
-              <p class="leading-relaxed text-base">30</p>
+              <p class="leading-relaxed text-base">{{ enviados }}</p>
             </div>
           </div>
           <div class="xl:w-1/3 md:w-1/2 p-4">
             <div class="border  bg-white border-gray-200 p-6 rounded-lg">
-              <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+              <div
+                  class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                 <svg class="svg-icon" viewBox="0 0 20 20">
-                  <path fill="none" d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z"></path>
+                  <path fill="none"
+                        d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z"></path>
                 </svg>
               </div>
               <h2 class="text-lg text-gray-900 font-medium title-font mb-2">recebidos</h2>
-              <p class="leading-relaxed text-base">30</p>
+              <p class="leading-relaxed text-base">{{ recebidos }}</p>
             </div>
           </div>
         </div>
       </div>
     </section>
 
+    <section class=" p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800 mt-2 mb-2">
+      <h2 class="text-lg font-semibold text-gray-700 capitalize dark:text-white">período</h2>
+
+      <form class="flex gap-2 ">
+        <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2 w-10/12">
+          <div>
+            <label class="text-gray-700 dark:text-gray-200" for="username">início do período </label>
+            <input id="username" type="date"
+                   class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300
+                   rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500
+                   dark:focus:border-blue-500 focus:outline-none focus:ring">
+          </div>
+
+          <div>
+            <label class="text-gray-700 dark:text-gray-200" for="emailAddress">fim do período</label>
+            <input id="emailAddress" type="date"
+                   class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md
+                   dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500
+                   dark:focus:border-blue-500 focus:outline-none focus:ring">
+          </div>
+        </div>
+
+        <div class="  mt-12 w-2/12">
+          <button
+              class="px-6 py-2 text-white transition-colors duration-200 transform bg-gray-700 rounded-md
+              hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+            pesquisar
+          </button>
+        </div>
+      </form>
+    </section>
+
     <section class="text-gray-600 body-font">
-        <div class="container  mx-auto">
+      <div class="container  mx-auto">
         <div class="flex flex-wrap -m-4">
           <div class="xl:w-1/2 md:w-1/2 p-4">
             <div class="border bg-white shadow border-gray-200 p-6 rounded-lg">
@@ -52,10 +92,25 @@
           </div>
           <div class="xl:w-1/2 md:w-1/2 p-4">
             <div class="border bg-white shadow border-gray-200 p-6 rounded-lg">
-              <testChart></testChart>
+              <lineChart></lineChart>
             </div>
           </div>
-
+        </div>
+      </div>
+    </section>
+    <section class="text-gray-600 body-font">
+      <div class="container  mx-auto">
+        <div class="flex flex-wrap -m-4">
+          <div class="xl:w-1/2 md:w-1/2 p-4">
+            <div class="border bg-white shadow border-gray-200 p-6 rounded-lg">
+              <productLineChart></productLineChart>
+            </div>
+          </div>
+          <div class="xl:w-1/2 md:w-1/2 p-4">
+            <div class="border bg-white shadow border-gray-200 p-6 rounded-lg">
+              <categoryLineChart></categoryLineChart>
+            </div>
+          </div>
         </div>
       </div>
     </section>
@@ -66,13 +121,90 @@
 <script>
 import AdminTemplate from "@/views/templates/AdminTemplate";
 import testChart from "@/components/charts/testChart";
+import lineChart from "@/components/charts/lineChart";
+import productLineChart from "@/components/charts/productLineChart";
+import categoryLineChart from "@/components/charts/categoryLineChart";
+import Cookie from "js-cookie";
 
 export default {
   name: "ProfileAdmin",
-  components: {AdminTemplate, testChart},
-  methods: {},
-  created() {
+  data() {
+    return {
+      recebidos: '',
+      enviados: '',
+      andamento: ''
+    }
+  },
+  components: {AdminTemplate, testChart, lineChart, categoryLineChart, productLineChart},
+  methods: {
+    loadPedidoAndamento() {
+      this.loading = true
+      let url = `/returnQuantifiedByStatus/andamento`
+      this.axios
+          .request({
+            url: url,
+            method: 'GET',
+            baseURL: 'http://localhost:8080/place/order',
+            headers: {
+              "Authorization": "Bearer  " + Cookie.get('token'),
+              "Access-Control-Allow-Origin": '*',
+              "Access-Control-Allow-Headers": "Origin, X-Request-Width, Content-Type, Accept",
+            }
 
+          })
+          .then(response => {
+            this.andamento = response.data
+            this.loading = false;
+          })
+    },
+    loadPedidoRecebidos() {
+      this.loading = true
+      let url = `/returnQuantifiedByStatus/recebido`
+      this.axios
+          .request({
+            url: url,
+            method: 'GET',
+            baseURL: 'http://localhost:8080/place/order',
+            headers: {
+              "Authorization": "Bearer  " + Cookie.get('token'),
+              "Access-Control-Allow-Origin": '*',
+              "Access-Control-Allow-Headers": "Origin, X-Request-Width, Content-Type, Accept",
+            }
+
+          })
+          .then(response => {
+            this.recebidos = response.data
+            this.loading = false;
+          })
+    },
+    loadPedidoEnviados() {
+      this.loading = true
+      let url = `/returnQuantifiedByStatus/enviado`
+      this.axios
+          .request({
+            url: url,
+            method: 'GET',
+            baseURL: 'http://localhost:8080/place/order',
+            headers: {
+              "Authorization": "Bearer  " + Cookie.get('token'),
+              "Access-Control-Allow-Origin": '*',
+              "Access-Control-Allow-Headers": "Origin, X-Request-Width, Content-Type, Accept",
+            }
+
+          })
+          .then(response => {
+            this.enviados = response.data
+            this.loading = false;
+          })
+    },
+    loadPurchaseQuantities() {
+      this.loadPedidoAndamento()
+      this.loadPedidoRecebidos()
+      this.loadPedidoEnviados()
+    }
+  },
+  created() {
+    this.loadPurchaseQuantities()
   }
 }
 </script>
